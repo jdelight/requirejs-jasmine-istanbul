b0d858c434e137d881a4306b09dfed91d981c652
James Da Costa <jamesdacosta@Esther-Da-Costas-MacBook.local>
2014-05-12 07:44:16 +0100

    initial working test with require and jasmine

--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,12 +1,12 @@
 define(['hello'], function(hello){
 
-  describe('hello', function(){
+    describe('hello', function(){
 
-    it('should do something', function(){
-      expect(true).toEqual(true);
-      expect(hello.init()).toEqual('hello world');
+        it('should do something', function(){
+            expect(true).toEqual(true);
+            expect(hello.init()).toEqual('hello world');
+        });
+
     });
-
-  });
 
 });
